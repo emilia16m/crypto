0b944cc79f3b31933a80a918cd42f93501285b7a
emilia16m <73434780+emilia16m@users.noreply.github.com>
2023-02-28 17:08:20 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,42 +1,20 @@
 import Descr from './components/descr'
-import { useEffect, useState } from 'react';
-import Axios from 'axios';
-import Dropdown from 'react-dropdown';
-import { HiSwitchHorizontal } from 'react-icons/hi';
 import './App.css';
-import ReactDropdown from 'react-dropdown';
-import Convert from './components/convert';
-import Left from './components/left';
+import Wrapper from './components/wrapper';
+import Forms from './components/form';
 
 function App() {
-
-return (
-	<div className="App">
-	<Descr />
-    
-  <div className='exchange'>
-      <div className='container'>
-        <Left />
-      </div>
-  </div> 
-
-
-
-  <div className="form">
-    <div className="container">
-      <div className='form_wrapp'>
-        <div className='input_wrapp'>
-          <label>Your Ethereum address</label>
-          <input></input>
-        </div>
-
-        <button className='btn_form'>Exchange</button>
-      </div>
+  return (
+    <div className="App">
+      <Descr />
+      <div className='exchange'>
+          <div className='container'>
+            <Wrapper />
+          </div>
+      </div> 
+      <Forms />
     </div>
-  </div> 
-	</div>
-
-
-)
+  )
 };
- export default App;
+
+export default App;
