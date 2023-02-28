fc94439c7fe6e48b180e9b39a5e210c0c228f218
emilia16m <73434780+emilia16m@users.noreply.github.com>
2023-02-28 17:11:41 +0300

    Create form.jsx

---
+++ src/components/form.jsx
@@ -0,0 +1,18 @@
+import React from "react"; 
+const Forms = () => {
+    return(
+        <div className="form">
+            <div className="container">
+            <div className='form_wrapp'>
+                <div className='input_wrapp'>
+                <label>Your Ethereum address</label>
+                <input></input>
+                </div>
+                <button className='btn_form'>Exchange</button>
+            </div>
+            </div>
+        </div> 
+    )
+}
+
+export default Forms;
